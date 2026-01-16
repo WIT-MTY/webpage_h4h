50f499c93ec2536c4573ff598b586b3196a767da
Khao <fatimacastilloag@gmail.com>
2026-01-16 00:08:07 -0600

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,33 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      
+      <p className="font-questrial text-4xl font-bold text-principal-rosa">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-50">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-100">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-200">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-300">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-400">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-500">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-600">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-700">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-rosa-800">HACK4HER</p>
+
+      <p>-----</p>
+      <p className="font-questrial text-4xl font-bold text-principal-morado">HACK4HER</p>
+      <p className="font-questrial text-4xl font-bold text-principal-morado-800">HACK4HER</p>
+      <p>-----</p>
+
+      <p className="font-questrial text-3xl font-bold text-secundario-morado">HACK4HER</p>
+      <p className="font-questrial text-3xl font-bold text-secundario-morado-200">HACK4HER</p>
+      <p>-----</p>
+      <p className="font-questrial text-3xl font-bold text-secundario-rosa">HACK4HER</p>
+      <p className="font-questrial text-3xl font-bold text-secundario-rosa-800">HACK4HER</p>
+      <p>-----</p>
+      <p className="font-questrial text-3xl font-bold text-secundario-rosa-claro">HACK4HER</p>
+      <p className="font-questrial text-3xl font-bold text-secundario-rosa-claro-800">HACK4HER</p>
+      
     </div>
-  );
+  )
 }
