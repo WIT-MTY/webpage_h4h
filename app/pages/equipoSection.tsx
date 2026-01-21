bee3b623226505fb08c3f46ad586610c40028f25
Khao <fatimacastilloag@gmail.com>
2026-01-20 19:11:59 -0600

    first commit test

--- app/pages/equipoSection.tsx
+++ app/pages/equipoSection.tsx
@@ -4,7 +4,7 @@ import Section from "../componentes/general/Section";
 const EquipoSection = () => {
     return (
         <Section id="equipo" backgroundColor="bg-secundario-rosa-600" className="h-full flex flex-col items-center justify-center">
-        <p className="text-secundario-morado text-2xl">Sección 7</p>
+        <p className="text-secundario-morado text-2xl">Equipo</p>
         <h2 className="text-3xl font-bold mt-4">Equipo</h2>
       </Section>
     )
