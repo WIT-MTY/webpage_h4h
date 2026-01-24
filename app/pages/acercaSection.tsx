1f86b772993b8a66a8a88915e60c1da03d4bd0cd
GalileaRestrepo <a01178273@tec.mx>
2026-01-24 14:13:27 -0600

    navbar fixed fixed

--- app/pages/acercaSection.tsx
+++ app/pages/acercaSection.tsx
@@ -3,9 +3,10 @@ import Section from "../componentes/general/Section";
 
 const AcercaSection = () => {
     return (
-        <Section id="acerca" backgroundColor="bg-secundario-rosa" className="h-full flex flex-col items-center justify-center">        <p className="text-secundario-morado text-2xl">Sección 1</p>
+        <Section id="acerca" backgroundColor="bg-secundario-rosa-50" className="h-full flex flex-col items-center justify-center">
+        <p className="text-secundario-morado text-2xl">Sección 1</p>
         <h2 className="font-questrial text-4xl font-bold text-principal-rosa">Acerca de</h2>
-        <h1 className="font-high-cruiser text-6xl text-morado-700">Otra fuente [High Cruiser] AA Aa aa 67</h1>
+        <h1 className="font-high-cruiser text-6xl text-morado-700">Otra fuente [High Cruiser] AA Aa aa</h1>
         <p> Subtitulos [Questrial] (por default)</p>
       </Section>
     )
