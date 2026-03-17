8c5035d3863055ba631826e672119dfe70e8369e
Khao <fatimacastilloag@gmail.com>
2026-03-16 18:45:03 -0600

    actualizción de componente Header: avance 2, base

--- app/componentes/navbar/header.tsx
+++ app/componentes/navbar/header.tsx
@@ -46,42 +46,29 @@ const Header = ({ onSectionChange }: HeaderProps) => {
     <header className="fixed top-0 w-full z-50 transition-all duration-300">
       <nav className="px-6 py-3 flex font-montserrat font-semibold text-white">
         
-        {isHome && <Logo image_logo="/images/wit_logos/logo_wit_blanco.png" w_logo="w-40" />}
+        <div className="hidden lg:block z-50"> {/* logo no visible en celular */}
+          {isHome && <Logo image_logo="/images/wit_logos/logo_wit_blanco.png" w_logo="w-40" />}
+        </div>
 
-        <button className="lg:hidden flex flex-col justify-center items-center w-10 h-10 " 
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-        >
-          <span className={`bg-white h-0.5 w-6 rounded-sm transition-all duration-300 ${isMenuOpen ? 'rotate-45 translate-y-2' : ''}`}></span>
-          <span className={`bg-white h-0.5 w-6 rounded-sm my-1.5 transition-all duration-300 ${isMenuOpen ? 'opacity-0' : ''}`}></span>
-          <span className={`bg-white h-0.5 w-6 rounded-sm transition-all duration-300 ${isMenuOpen ? '-rotate-45 -translate-y-2' : ''}`}></span>
+        <button onClick={() => setIsMenuOpen(!isMenuOpen)}className="lg:hidden text-white focus:outline-none z-50">
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            {isMenuOpen ? (
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                ) : (
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            )}
+          </svg>
         </button>
         
-
-
         <div 
-          className={`
-    /* Estilos para Celular (Mobile) */
-    ${isMenuOpen ? "fixed inset-0 flex flex-col items-center justify-center z-40" : "hidden"}
-    
-    /* Estilos para Computadora (Escritorio) */
-    lg:flex lg:absolute lg:left-1/2 lg:-translate-x-1/2 lg:p-6 lg:py-1 
-    lg:rounded-r-4xl lg:rounded-s-4xl lg:transition-all lg:duration-300
-  `}
-          
-          style={{ 
-            backgroundColor: isHome ? "transparent": (isHome ? "transparent" : "#703D5C")
-          }}
+          className={`${isMenuOpen ? "flex" : "hidden"}
+            lg:flex lg:absolute lg:left-1/2 lg:-translate-x-1/2 lg:p-6 lg:py-1 
+            lg:rounded-r-4xl lg:rounded-s-4xl lg:transition-all lg:duration-300`}
+            style={{backgroundColor: isHome ? "transparent": (isHome ? "transparent" : "#703D5C")}}
         >
           <div className="flex flex-col lg:flex-row items-center space-y-8 lg:space-y-0 lg:space-x-6">
             {!isHome && (
-              <Link 
-                href="#home" 
-                className="hover:text-secundario-morado-700 transition-colors"
-                onClick={() => setIsMenuOpen(false)}
-                scroll={true}
-              >
-                Inicio
-              </Link>
+            <Link href="#home" onClick={() => setIsMenuOpen(false)} className="hover:text-secundario-morado-700 transition-colors" scroll={true}>Inicio</Link>
             )}
             
             <Link href="#acerca" onClick={() => setIsMenuOpen(false)} className="hover:text-secundario-morado-700 transition-colors"> Acerca </Link>
@@ -93,6 +80,7 @@ const Header = ({ onSectionChange }: HeaderProps) => {
             <Link href="#contactanos" onClick={() => setIsMenuOpen(false)} className="hover:text-secundario-morado-700 transition-colors"> Contáctanos </Link>
           </div>
         </div>
+
       </nav>
     </header>
   );
