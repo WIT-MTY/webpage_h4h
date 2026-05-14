b5350bae7eab9b0b55b7bb079f4efdd79de41b6c
Khao <fatimacastilloag@gmail.com>
2026-05-13 19:58:01 -0600

    added: vista para recuperación de contrasenia

--- app/registro/iniciosesion/page.tsx
+++ app/registro/iniciosesion/page.tsx
@@ -4,12 +4,14 @@ import { useRouter } from "next/navigation";
 import HeaderForms from "@/app/componentes/formulario_comp/HeaderForms";
 import BackgroundDecor from "@/app/componentes/general/BackgroundDoodles";
 
+type Paso = 'iniciosesion' | 'recuperacion' 
 
 export default function PageFormulario() {
     
     const router = useRouter();
 
     const [correo, setCorreo] = useState("");
+    const [paso, setPaso] = useState<Paso>('iniciosesion')
     const [contrasena, setContrasena] = useState("");
     const [errores, setErrores] = useState<Record<string, string>>({});
     const [errorGeneral, setErrorGeneral] = useState("");
@@ -75,6 +77,9 @@ export default function PageFormulario() {
                 
                 
                 <main className="w-full max-w-md mx-auto">
+
+                     {/* Inicio de sesion */}
+                    {paso === 'iniciosesion' && (
                     <form className="space-y-6 pb-20 mt-8" onSubmit={handleSubmit}>
                         {/* Campos de datos en disposición vertical */}
                         <div className="bg-white/10 p-8 rounded-lg">
@@ -123,6 +128,11 @@ export default function PageFormulario() {
                                 {errores.contrasena && <p className="text-red-400 text-sm mt-1">{errores.contrasena}</p>}
                             </div>
                                 {errorGeneral && <p className="text-red-400 text-sm text-center">{errorGeneral}</p>}
+                                <button 
+                                        onClick={() => setPaso('recuperacion')} 
+                                        className="flex items-center space-x-2 text-[#bd699c] underline hover:text-pink-600 transition-colors text-lg font-medium group">
+                                        ¿Olvidaste tu contraseña?
+                                    </button>
                             </div>
                         </div>
 
@@ -135,6 +145,77 @@ export default function PageFormulario() {
                             Iniciar Sesión
                         </button>
                     </form>
+                    )}
+
+                    {/* Recuperar contrasenia */}
+                    {paso === 'recuperacion' && (
+                    <form className="space-y-6 pb-20 mt-8" >
+                        {/* Campos de datos en disposición vertical */}
+                        <div className="bg-white/10 p-8 rounded-lg">
+                            <h2 className="text-white text-xl font-semibold mb-4">¿Olvidaste tu contraseña?</h2>
+                            
+                            {/* Disposición vertical en lugar de grid */}
+                            <div className="flex flex-col space-y-4">
+                                <div>
+                                    <p className="text-white mb-2">Correo Electrónico <span className="text-red-400">*</span></p>
+                                    <input 
+                                        type="email" 
+                                        value={correo}
+                                        onChange={(e) => setCorreo(e.target.value)}
+                                        className="w-full p-3 rounded-md bg-white text-black" 
+                                        placeholder="Ingresa tu correo" 
+                                    />
+                                    {errores.correo && <p className="text-red-400 text-sm mt-1"></p>}
+                                </div>
+                                <div>
+                                    <p className="text-white mb-2">Contraseña <span className="text-red-400">*</span></p>
+                                    <div className="relative">
+                                        <input 
+                                         
+                                            className="w-full p-3 rounded-md bg-white text-black pr-12" 
+                                            placeholder="Ingresa una contraseña" 
+                                        />
+                                        <button
+                                            type="button"
+                                            
+                                            className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                                        >
+                                            {mostrarContrasena ? (
+                                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                            </svg>
+                                        ) : (
+                                            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 4.411m0 0L21 21" />
+                                            </svg>
+                                        )}
+                                    </button>
+                                </div>
+               
+                            </div>
+                            <button 
+                                onClick={() => setPaso('iniciosesion')} 
+                                className="flex items-center space-x-2 text-[#bd699c] underline hover:text-pink-600 transition-colors text-lg font-medium group">
+                                Iniciar sesión
+                            </button>
+
+                         
+                            </div>
+                        </div>
+
+                        {/* Botón de recuperacion de contrasenia */}
+                        <button 
+                            type="submit" 
+                            className="w-full text-white font-montserrat text-base sm:text-lg md:text-xl font-semibold rounded-lg transition-all duration-300 hover:scale-[1.02] active:scale-[0.98] py-4 px-6 border-[3px]"
+                            style={{ backgroundColor: '#4F123F', borderColor: '#4F123F' }}
+                        >
+                            Envíar correo de recuperación
+                        </button>
+                    </form>
+                    )}
+
+
                 </main>
             </div>
         </section>
